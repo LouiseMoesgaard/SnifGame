d5a7b83729581ad6482a3a02e89637b13f72a474
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-10-13 23:03:30 +0200

    heat powerloop done

--- heat.js
+++ heat.js
@@ -1,10 +1,55 @@
-
 "use strict";
 
-// const warmth = {
-//     items: ["window1", "Window2", "radiater1", "radiater2", "snif", "hunter"]
-// }
+window.radiatorLeft = {
+    onElement:"#heat_radi_left_on",
+    offElement:"#heat_radi_left_off",
+    heatOn: true
+}
+
+window.radiatorRight = {
+    onElement:"#heat_radi_right_on",
+    offElement:"#heat_radi_right_off",
+    heatOn: false
+}
+
+const heatInputs = {
+    radiatorLeftClicked: false,
+    radiatorRightClicked: false
+}
 
 async function heatInit(){
-    document.querySelector("#game").innerHTML = await getSVG("heat");
+    game.points = 0;
+    document.querySelector("#game").innerHTML = await getSVG("heat"); //får det returnede SVG
+    heatEvents();
+    heatLoop();
+}
+
+function heatEvents() {
+    document.querySelector(radiatorLeft.onElement).addEventListener("click", ()=> heatInputs.radiatorLeftClicked = true);
+    document.querySelector(radiatorRight.offElement).addEventListener("click", ()=> heatInputs.radiatorRightClicked = true);
+}
+
+function heatLoop(){
+    Object.keys(heatInputs).forEach(key =>{
+
+        if(heatInputs[key]){
+            window[key.split("Clicked")[0]].heatOn = !window[key.split("Clicked")[0]].heatOn;
+            heatInputs[key] = false;
+            game.points++
+            heatRenderModel(window[key.split("Clicked")[0]]);
+            winCondition("heat");
+        }    
+    })
+    requestAnimationFrame(heatLoop);
+}
+
+function heatRenderModel(model){
+    if(model.heatOn){
+        document.querySelector(model.offElement).classList.add("hide");
+        document.querySelector(model.onElement).classList.remove("hide");
+    } else{
+        document.querySelector(model.onElement).classList.add("hide");
+        document.querySelector(model.offElement).classList.remove("hide");
+    }
+
 }
